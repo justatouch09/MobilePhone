31b5a1f9ef702b32f8cc0e34ec2125882a6a3db8
Jarad Touchberry <you@example.com>
2017-05-19 14:10:04 -0400

    arraylistchallenge

---
+++ src/com/company/Contact.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * Created by jaradtouchberry on 5/18/17.
+ */
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+}
